6c7d2a196d6c5f4a5399ad99fed85b42abd9cd1c
Johannes Brandt <jbrandt@2-F-7.42heilbronn.de>
2025-01-18 17:44:32 +0100

    added more libraries to the header.

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 13:53:34 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/01/18 13:56:30 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/01/18 17:44:09 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
 # include <stdlib.h>
 # include <math.h>
 # include <unistd.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <sys/stat.h>
 
 #endif
