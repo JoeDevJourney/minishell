534abe2edaec7e6002896c571686f06c215d6f03
Dimitris Chrysovergis <dchrysov@2-G-8.42heilbronn.de>
2025-01-20 19:01:03 +0100

    CHECKPOINT: (n/w) basic echo function

--- src/builtins.c
+++ src/builtins.c
@@ -1,18 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:45:20 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/10/19 23:23:18 by dimitris         ###   ########.fr       */
+/*   Updated: 2025/01/20 18:58:47 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// int	echo()
+int	echo(char **str, bool upper)
+{
+	char	**ptr;
+	// char	*line;
+
+	(void) upper;
+	ptr = str;
+	while (*(ptr + 1))
+	// if (ft_strncmp(str[0], "-n", ft_strlen(str[0])))
+	if (!ft_strncmp(str[0], "-n", ft_strlen(str[0])))
+	{
+		printf("\'%s \'", *++ptr);
+		printf("\n");
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	*str = "-n Hello mr malaka";
+	char	**token;
+	int		res;
+
+	(void)res;
+	token = ft_split(str, ' ');
+	// while (*ptr)
+	// 	printf("token[%d]: \'%s\'\n", j++, *ptr++);
+	res = echo(token, false);
+}
 
 // int	cd()
 
