3863ae0cb4c6fdc55c0f5f9e8f1595a3a91b2c53
dchrysov <j.chrisov@gmail.com>
2025-01-24 01:12:31 +0100

    copy src/externals.c from external branch

---
+++ src/externals.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   externals.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 15:42:19 by dchrysov          #+#    #+#             */
+/*   Updated: 2025/01/23 16:41:37 by dchrysov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+static int	execute_command(char *cmd, char *argv[], char *envp[])
+{
+	pid_t	pid;
+	int		res;
+
+	pid = fork();
+	res = 0;
+	if (pid == 0)
+	{
+		res = execve(cmd, argv, envp);
+		exit(errno);
+	}
+	else if (pid > 0)
+		wait(NULL);
+	else
+		perror("Fork failure");
+	return (res);
+}
+
+static char	*join_cmd(char **arr)
+{
+	char	*temp;
+	char	*line;
+	char	*newline;
+
+	temp = NULL;
+	newline = NULL;
+	line = NULL;
+	while (*arr)
+	{
+		temp = ft_strjoin(*arr++, " ");
+		newline = ft_strjoin(line, temp);
+		free(temp);
+		free (line);
+		line = newline;
+	}
+	return (line);
+}
+
+static void	free_array(char **arr)
+{
+	char	**temp;
+
+	temp = arr;
+	while (*temp)
+	{
+		free(*temp);
+		temp++;
+	}
+	free (arr);
+}
+
+/**
+ * @brief Executes all external commands (executables located at /bin/)
+ * 
+ * @paragraph str command to be executed, broken down in tokens.
+ */
+void	externals(char **str)
+{
+	char	**input;
+	char	*cmd;
+
+	cmd = join_cmd(str);
+	input = ft_split(cmd, ' ');
+	free(input[0]);
+	*input = ft_strjoin("/bin/", *str);
+	execute_command(*input, input, NULL);
+	free_array (input);
+	free (cmd);
+}
+
+// int	main(int argc, char **argv)
+// {
+// 	(void)argc;
+// 	externals(++argv);
+// }
+
+	// cc externals.c ../include/libft/src/ft_strlen.c ../include/libft/src/ft_split.c ../include/libft/src/ft_strlcpy.c ../include/libft/src/ft_strjoin.c ../include/libft/src/ft_strlcat.c -g -Wall -Werror -Wextra
