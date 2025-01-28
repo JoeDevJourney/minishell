34325838c488b5a4b522868768f10e02405558aa
Dimitris Chrysovergis <dchrysov@2-F-5.42heilbronn.de>
2025-01-28 19:22:02 +0100

    CHECKPOINT: (W) parsing.c Norminette changes

--- src/parsing.c
+++ src/parsing.c
@@ -6,63 +6,68 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:45:20 by dchrysov          #+#    #+#             */
-/*   Updated: 2025/01/28 11:20:23 by dchrysov         ###   ########.fr       */
+/*   Updated: 2025/01/28 12:40:05 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static char *substr_dup(const char *start, size_t len)
+static char	*substr_dup(const char *start, size_t len)
 {
-    char *substr = (char *)malloc(len + 1);
-    if (!substr)
-        return (NULL);
-    ft_strlcpy(substr, start, len);
-    substr[len] = '\0';
-    return (substr);
+	char	*substr;
+
+	substr = (char *)malloc(len + 1);
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, start, len);
+	substr[len] = '\0';
+	return (substr);
 }
 
-size_t count_substr(const char *s, const char *delim)
+size_t	count_substr(const char *s, const char *delim)
 {
-    size_t count = 0;
-    const char *tmp = s;
-    size_t delim_len;
+	size_t		count;
+	const char	*tmp = s;
+	size_t		delim_len;
 
+	count = 0;
 	delim_len = strlen(delim);
-    while ((tmp = ft_strnstr(tmp, delim, ft_strlen(tmp))))
-    {
-        count++;
-        tmp += delim_len;
-    }
-    return count + 1;
+	tmp = ft_strnstr(tmp, delim, ft_strlen(tmp));
+	while (tmp)
+	{
+		count++;
+		tmp += delim_len;
+	}
+	return (count + 1);
 }
 
 /**
  * @brief Works in the same way as the original split, with the diff now being
  * that the delimeter is a char* instead of a char.
  */
-char **ft_split2(const char *s, const char *delim)
+char	**ft_split2(const char *s, const char *delim)
 {
-    char **result;
-    size_t i;
-    size_t delim_len;
-    const char *start = s;
-    const char *end;
+	char		**result;
+	const char	*start;
+	const char	*end;
+	size_t		delim_len;
+	size_t		i;
 
 	i = 0;
+	start = s;
 	delim_len = ft_strlen(delim);
-    if (!s || !delim)
-        return (NULL);
-    result = (char **)malloc((count_substr(s, delim) + 1) * sizeof(char *));
-    if (!result)
-        return (NULL);
-    while ((end = ft_strnstr(start, delim, ft_strlen(start))))
-    {
-        result[i++] = substr_dup(start, end - start);
-        start = end + delim_len;
-    }
-    result[i++] = ft_strdup(start);
-    result[i] = NULL;
-    return (result);
+	end = ft_strnstr(start, delim, ft_strlen(start));
+	if (!s || !delim)
+		return (NULL);
+	result = (char **)malloc((count_substr(s, delim) + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	while (end)
+	{
+		result[i++] = substr_dup(start, end - start);
+		start = end + delim_len;
+	}
+	result[i++] = ft_strdup(start);
+	result[i] = NULL;
+	return (result);
 }
-
