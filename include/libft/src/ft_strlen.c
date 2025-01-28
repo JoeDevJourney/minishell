f0850e95fdbe1b681c206fb5eb02e682c54973e1
Dimitris Chrysovergis <dchrysov@2-F-5.42heilbronn.de>
2025-01-28 16:18:30 +0100

    strlen: Handled the str=NULL edge case

--- include/libft/src/ft_strlen.c
+++ include/libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:03:59 by dchrysov          #+#    #+#             */
-/*   Updated: 2025/01/21 15:30:46 by dchrysov         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:17:22 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ unsigned long	ft_strlen(const char str[])
 	const char		*ptr;
 	unsigned long	len;
 
+	if (!str)
+		return (0);
 	ptr = str;
 	len = 0;
 	if (str)
