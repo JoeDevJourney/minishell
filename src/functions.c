19a5b6b2ff3284baa2f78ddfa1eef16043e1d30d
Dimitris Chrysovergis <dchrysov@2-F-5.42heilbronn.de>
2025-01-31 14:45:28 +0100

    CHECKPOINT: (W) Pulled src/ from master

---
+++ src/functions.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 15:13:09 by dchrysov          #+#    #+#             */
+/*   Updated: 2025/01/30 18:57:13 by dchrysov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	*safe_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+	{
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	return (ptr);
+}
+
+char	*relative_wd(char *dir)
+{
+	char	*ptr;
+
+	ptr = dir;
+	ptr += ft_strlen(dir) - 1;
+	while (ptr-- && *ptr != '/')
+		ptr--;
+	return (++ptr);
+}
