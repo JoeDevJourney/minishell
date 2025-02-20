23ff92c23de45a58266c63a53d2f5456bbd2d14e
Johannes Brandt <jbrandt@3-G-3.42heilbronn.de>
2025-02-20 18:51:26 +0100

    modified the header. Got rid of some functions that are already handled before

--- src/builtins/cd/cd_utils3.c
+++ src/builtins/cd/cd_utils3.c
@@ -6,58 +6,11 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:10:49 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/02/05 16:58:05 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/02/20 18:47:13 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-int	is_builtin(char *cmd)
-{
-	const char	*builtins[] = {"cd", "exit", NULL};
-	int			i;
-
-	i = 0;
-	while (builtins[i])
-	{
-		if (ft_strcmp(cmd, builtins[i]) == 0)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	execute_builtin(char **env, char **args)
-{
-	char	*builtins[3];
-	int		i;
-
-	builtins[0] = "cd";
-	builtins[1] = "exit";
-	builtins[2] = NULL;
-	i = 0;
-	while (builtins[i] != NULL)
-	{
-		if (ft_strcmp(args[0], builtins[i]) == 0)
-			break ;
-		i++;
-	}
-	if (ft_strcmp(builtins[i], "cd") == 0)
-		return (ft_cd(env, args));
-	else if (ft_strcmp(builtins[i], "exit") == 0)
-		return (ft_exit(args));
-	return (127);
-}
-
-int	ft_exit(char **args)
-{
-	int	status;
-
-	status = 0;
-	if (args[1])
-		status = ft_atoi(args[1]);
-	exit (status);
-}
 
 static char	*process_argument(char *arg)
 {
