3bc9e4f7ba45d2847a9bb2ee14773ff6a895f42c
Johannes Brandt <jbrandt@3-G-3.42heilbronn.de>
2025-02-20 17:25:32 +0100

    copied the entire cd folder from branch export

---
+++ src/builtins/cd/cd_main.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_main.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 14:33:43 by jbrandt           #+#    #+#             */
+/*   Updated: 2025/02/05 16:57:46 by jbrandt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+char	*create_env_entry(const char *name, const char *value)
+{
+	char	*new_entry;
+	size_t	len;
+
+	len = ft_strlen(name) + ft_strlen(value) + 2;
+	new_entry = malloc(len);
+	if (!new_entry)
+	{
+		perror("cd: malloc failed\n");
+		exit (EXIT_FAILURE);
+	}
+	ft_strcpy(new_entry, name);
+	ft_strcat(new_entry, "=");
+	ft_strcat(new_entry, value);
+	return (new_entry);
+}
+
+int	replace_env_var(char **env, const char *name, char *new_entry)
+{
+	int		i;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], name, ft_strlen(name)) == 0 \
+			&& env[i][ft_strlen(name)] == '=')
+		{
+			env[i] = new_entry;
+			return (0);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+int	update_env_var(char **env, const char *name, const char *value)
+{
+	char	*new_entry;
+
+	new_entry = create_env_entry(name, value);
+	if (!new_entry)
+		return (1);
+	if (replace_env_var(env, name, new_entry) == 0)
+		return (0);
+	if (add_env_var(env, new_entry) != 0)
+		return (1);
+	return (0);
+}
+
+int	ft_cd(char **env, char **args)
+{
+	char	*dir;
+	char	*oldpwd;
+
+	oldpwd = getenv("PWD");
+	dir = get_target_dir(args);
+	if (!dir || chdir(dir) != 0)
+		return (perror("cd failed"), 1);
+	if (update_pwd_vars(env, oldpwd) != 0)
+		return (1);
+	return (0);
+}
---
+++ src/builtins/cd/cd_utils3.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_utils3.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:10:49 by jbrandt           #+#    #+#             */
+/*   Updated: 2025/02/05 16:58:05 by jbrandt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int	is_builtin(char *cmd)
+{
+	const char	*builtins[] = {"cd", "exit", NULL};
+	int			i;
+
+	i = 0;
+	while (builtins[i])
+	{
+		if (ft_strcmp(cmd, builtins[i]) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	execute_builtin(char **env, char **args)
+{
+	char	*builtins[3];
+	int		i;
+
+	builtins[0] = "cd";
+	builtins[1] = "exit";
+	builtins[2] = NULL;
+	i = 0;
+	while (builtins[i] != NULL)
+	{
+		if (ft_strcmp(args[0], builtins[i]) == 0)
+			break ;
+		i++;
+	}
+	if (ft_strcmp(builtins[i], "cd") == 0)
+		return (ft_cd(env, args));
+	else if (ft_strcmp(builtins[i], "exit") == 0)
+		return (ft_exit(args));
+	return (127);
+}
+
+int	ft_exit(char **args)
+{
+	int	status;
+
+	status = 0;
+	if (args[1])
+		status = ft_atoi(args[1]);
+	exit (status);
+}
+
+static char	*process_argument(char *arg)
+{
+	char	*dir;
+	char	*home;
+
+	if (ft_strcmp(arg, "-") == 0)
+	{
+		dir = get_oldpwd_dir();
+		if (dir)
+		{
+			printf("%s\n", dir);
+			dir = ft_strdup(dir);
+		}
+		return (dir);
+	}
+	if (arg[0] == '~')
+	{
+		home = get_home_dir();
+		if (!home)
+			return (NULL);
+		dir = ft_strjoin(home, arg + 1);
+		return (dir);
+	}
+	if (arg[0] == '\0')
+		return (NULL);
+	return (ft_strdup(arg));
+}
+
+char	*get_target_dir(char **args)
+{
+	char	*home;
+
+	if (args[1] && args[2])
+		return (NULL);
+	if (!args[1])
+	{
+		home = get_home_dir();
+		if (home)
+			return (ft_strdup(home));
+		return (NULL);
+	}
+	return (process_argument(args[1]));
+}
