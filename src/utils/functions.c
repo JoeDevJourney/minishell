8379834d4393af85827ccab06965ce4cba2bc839
dchrysov <j.chrisov@gmail.com>
2025-02-25 23:56:39 +0100

    Changes to be committed: 	modified:   src/utils/functions.c 	deleted:    src/utils/quote_utils Fixed the leaks merged from master

--- src/utils/functions.c
+++ src/utils/functions.c
@@ -62,19 +62,26 @@ bool	valid_oper(char **str, char *dl)
 	arr = ft_split2(*str, dl);
 	size = count_array_size(arr);
 	i = -1;
-	while (++i < size - 1)
-		if (*ft_strtrim(arr[i], " ") == '\0')
-			return (printf("syntax error near unexpected token `%s'\n", dl), 0);
-	if (*ft_strtrim(arr[i], " ") == '\0')
+	while (++i < size)
 	{
-		cmd = readline("> ");
-		while (!*cmd)
-			cmd = readline("> ");
-		ptr = ft_strjoin(*str, cmd);
-		free(*str);
-		*str = ft_strdup(ptr);
+		ptr = ft_strtrim(arr[i], " ");
+		if (*ptr == '\0')
+		{
+			if (i < size - 1)
+				return (printf("syntax error near unexpected token `%s'\n", dl), 0);
+			else
+			{
+				cmd = readline("> ");
+				while (!*cmd)
+					cmd = readline("> ");
+				free(ptr);
+				ptr = ft_strjoin(*str, cmd);
+				free(*str);
+				*str = ft_strdup(ptr);
+				free(cmd);
+			}
+		}
 		free(ptr);
-		free(cmd);
 	}
 	return (free_array(arr), 1);
 }
