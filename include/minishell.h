210bf76c5bb833750e3f9db259696af83e2e26c9
Johannes Brandt <jbrandt@2-F-7.42heilbronn.de>
2025-03-18 14:46:29 +0100

    fixed return value with signals. Still couldnt fix the thing with control D that its printing that new line thanks to readlin

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:38:05 by dchrysov          #+#    #+#             */
-/*   Updated: 2025/03/18 13:56:52 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/03/18 14:32:46 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,6 @@ typedef struct s_data
 	t_redir_op		hdoc_op;
 	int				ret_val;
 	int				in_heredoc;
-	bool			is_heredoc;
 }			t_data;
 
 //	Execution
