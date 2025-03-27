1c7c8ae81f171195604cab78aecb4271398c9f87
Dimitris Chrysovergis <dchrysov@1-D-17.42heilbronn.de>
2025-03-27 15:08:40 +0100

    norminette

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 10:19:43 by dchrysov          #+#    #+#             */
-/*   Updated: 2025/03/27 14:06:26 by dchrysov         ###   ########.fr       */
+/*   Updated: 2025/03/27 15:06:12 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,9 +96,6 @@ static bool	valid_oper(t_data *inp, char *del)
 
 	arr = ft_split2(inp->cmd, del);
 	size = count_array_size(arr);
-	// while (*arr)
-	// 	printf("'%s'\n", *arr++);
-	// pause();
 	i = -1;
 	while (++i < size)
 	{
